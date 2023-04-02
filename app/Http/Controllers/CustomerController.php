a86efcc12424c61787b1e9d8ecf6a34439e922c6
Usama Mehmood <usama.mehmood@rolustech.net>
2023-04-02 22:37:25 +0500

    Customer model along with sign up, login, logout functionalities

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Customer;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Log;
+
+class CustomerController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:customers',
+            'password' => 'required|min:8',
+            'phone' => 'required',
+        ]);
+
+        $customer = Customer::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'phone' => $request->phone,
+        ]);
+
+        $token = $customer->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        // Log::info('Credentials: ' . json_encode($credentials)); // Log credentials
+
+        // if (!Auth::attempt($credentials)) {
+        //     throw ValidationException::withMessages([
+        //         'email' => ['The provided credentials are incorrect.'],
+        //     ]);
+        // }
+
+        $customer = Customer::where('email', $request->email)->first();
+
+        // Compare plain-text passwords
+        if (!$customer || $request->password !== $customer->password) {
+            return response()->json(['message' => 'Invalid email or password'], 401);
+        }
+
+        // Revoke existing tokens
+        $customer->tokens()->where('name', 'auth_token')->delete();
+
+        // Create a new token
+        $token = $customer->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'customer' => $customer,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        // Revoke the customer's current access token
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logout successful',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('signup', [CustomerController::class, 'store']);
+
+Route::post('login', [CustomerController::class, 'login']);
+
+Route::middleware('auth:sanctum')->post('/customer/logout', [CustomerController::class, 'logout']);
+
